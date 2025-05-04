dc8e2666ca8607a59dce8567e73751317b892a12
shashank-poola <shashankpoola123@gmail.com>
2025-05-04 09:00:58 +0530

    first commit of zentrix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-orange-500'>Shashank is a progammer</h1>
+      <h2>connected using with something new in the world and shashank</h2>
+    </div>
+  )
+}
+
+export default App
